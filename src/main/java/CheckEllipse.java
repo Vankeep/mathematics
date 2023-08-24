dc97f9972d4b39151d4b9a14461c5d032436e5f1
Ivan Mashkov <mashkovvan93@yandex.ru>
2023-08-25 00:08:48 +0600

    Точка в эллипсе

--- src/main/java/CheckEllipse.java
+++ src/main/java/CheckEllipse.java
@@ -3,24 +3,21 @@
 class CheckEllipse {
 
     public static void main(String arr[]) {
-        float h = 5, k = 10, a = 4, b = 6;
+        float xE = 5, yE = 10, w = 4,h = 6;
         for (int x = 0; x < 12; x++) {
             for (int y = 0; y < 20; y++) {
-                if (checkpoint(h, k, x, y, a, b) <= 1)
+                if (checkpoint(xE, yE, x, y, w, h) <= 1)
                     System.out.print(" 0 ");
                 else
                     System.out.print(" - ");
             }
             System.out.println();
         }
-//        else if (checkpoint(h, k, x, y, a, b) == 1)
+//      if (checkpoint(h, k, x, y, a, b) == 1)
 //            System.out.println("На эллипсе");
     }
-    static float checkpoint(float h, float k, float x, float y, float a, float b) {
-        float p = (float) ((Math.pow((x - h), 2) / Math.pow(a, 2))
-                + (Math.pow((y - k), 2) / Math.pow(b, 2)));
-
-        return p;
+    static float checkpoint(float xE, float yE, float x, float y, float w, float h) {
+        return (float) ((Math.pow((x - xE), 2) / Math.pow(w, 2)) + (Math.pow((y - yE), 2) / Math.pow(h, 2)));
     }
 
 }
